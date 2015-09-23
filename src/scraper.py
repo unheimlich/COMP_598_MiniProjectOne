9ec9155ed5b18585fd88eb7dc809a8e79b2c51dc
Kyle Brumsted <kylebrumsted@wpa048113.wireless.mcgill.ca>
2015-09-23 17:27:00 -0400

    scraper finished

--- src/scraper.py
+++ src/scraper.py
@@ -7,47 +7,81 @@
 # import libraries 
 import urllib, htmllib, re, sys
 from bs4 import BeautifulSoup
+from sets import Set
+
+
 
 # grab URL string from command line arguments
-url = sys.argv[1]
+url = "http://www.dailymail.co.uk/home/index.html"
 
-# open connection to website
-website = urllib.urlopen("http://"+url)
-# read html into the data variable
-data = website.read()
-# close the connection
-website.close()
-
-#destination for scraped files
-path = "/Users/kylebrumsted/Documents/School/Fall2015/COMP598/Project1/scraped/"
-
-#name for scraped files
+#declare global variables and structures
+unscrapedLinks = set([url])
+scrapedLinks = set([])
 counter = 0
-fname = path+str(counter)+".html"
+limit = 200
+path = "/Users/kylebrumsted/Documents/School/Fall2015/COMP598/Project1/scraped/"
+urlPrefix = "http://www.dailymail.co.uk"
 
-#Open new file, make first line the URL then write the entire HTML file
-htmlfile = open(fname, "w")
-htmlfile.write("<!--"+url+"-->")
-htmlfile.write(data)
-htmlfile.close()
+#as long as we have links we haven't explored, keep iterating
+while unscrapedLinks:
+	# quit loop after scraping to a limit
+	if len(scrapedLinks) > limit: break
 
-'''
-TODO: Gather anchor list, scrape each ref, increment the counter on each one. Also need to create a list of scraped links.
+	# remove top item from set
+	url = unscrapedLinks.pop()
 
-soup = BeautifulSoup(data, "html.parser")
-for link in soup.find_all('a'):
-
-	ref = link.get('href')
-
-	if ref == None:
+	# try to open connection to website
+	try:
+		website = urllib.urlopen(url)
+	except IOError:
+		print("[Scraper]:: Encountered malformed URL: "+url+". Will continue scraping...")
+		scrapedLinks.add(url)
 		continue
-	
-	#All the links that end in .html are from dailymail; all others go to external sites.
-	if '.html' not in ref:
-		continue
-	
- 	print(ref)
- 	#These refs all need http://www.dailymail.co.uk prefix. Make sure to account for this in scraping and in
- '''
+	# read html into the data variable
+	data = website.read()
+	# close the connection
+	website.close()
+
+	# we only save the page if it is an article. 
+	if 'article' in url:
+		# name for scraped files
+		fname = path+str(counter)+".html"
+		counter += 1
+
+		# open new file, make first line the URL then write the entire HTML file
+		htmlfile = open(fname, "w")
+		htmlfile.write("<!--"+url+"-->\n")
+		htmlfile.write(data)
+		htmlfile.close()
+		print("Saved article #"+str(counter)+": "+url)
+
+	# to scraped list
+	scrapedLinks.add(url)
+
+	# even if we didn't save, we still want all the links on the page
+	soup = BeautifulSoup(data, "html.parser")
+	#find all anchors and add them to the unscraped set
+	for link in soup.find_all('a'):
+		# get the ref
+		ref = link.get('href')
+
+		# filter the anchor list with some basic patterns
+		if ref == None: continue
+		if '.html' not in ref: continue
+		if 'dailymail' in ref: continue
+		if '#' in ref: continue
+		if 'www' in ref: continue
+		if 'http' in ref: continue
+
+		# add the dailymail prefix to the anchor
+		childURL = urlPrefix+ref
+		#if we already scraped that link, don't add.
+		if childURL in scrapedLinks: continue
+		unscrapedLinks.add(childURL)
+
+print("Number of unscraped links upon exit: "+str(len(unscrapedLinks)))
+		
+
+ 	
 
 
