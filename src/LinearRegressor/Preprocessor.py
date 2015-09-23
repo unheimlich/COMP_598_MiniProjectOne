dc2b065b2f9ad6460761f5b1ed70cdc95c209a08
andrew <andrew.d.jesson@gmail.com>
2015-09-23 16:04:40 -0400

    Normalized Gradient Descent Update

--- src/LinearRegressor/Preprocessor.py
+++ src/LinearRegressor/Preprocessor.py
@@ -5,3 +5,28 @@ class Preprocessor:
 
     def __init__(self):
 
+        self.mu = 0
+        self.sigma = 0
+        self.L = 0
+        self.V = 0
+
+
+    #ToDo: learn principal components and principal eigen values
+    def train(self,X):
+
+        self.mu = np.nanmean(X, axis=0, dtype=np.float64)
+        self.sigma = np.nanstd(X, axis=0, dtype=np.float64)
+
+    def center(self, X):
+
+        return X - self.mu
+
+    def standardize(self, X):
+
+        return (X - self.mu)/self.sigma
+
+    #ToDo: enable
+    def whiten(self, X):
+
+        S = (X - self.mu)/self.sigma
+        return S
